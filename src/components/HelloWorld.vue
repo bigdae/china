8c9aa658623682203e1d2167077e82b333aaef2d
권성대 <gwonseongdae@gwonseongdaeui-iMac.local>
2019-02-19 23:07:56 +0900

    modify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -22,12 +22,12 @@
                   :key="item.title"
                   @click="playFile(item.mp3url, index)">
 
-                  <v-list-tile-content>
-                    <v-list-tile-title v-html="item.title"></v-list-tile-title>
-                    <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                  <v-list-tile-content>                
                     <v-list-tile-sub-title v-html="item.kor"></v-list-tile-sub-title>                    
-                  </v-list-tile-content>
-                </v-list-tile>
+                    <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                  </v-list-tile-content>        
+                </v-list-tile> 
+
                 <v-divider :key="index"></v-divider>
                 <!--
                 <v-divider :key="index"></v-divider>
